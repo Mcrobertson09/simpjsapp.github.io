a5b758f92f406f1380a0e2a187471461f545b459
Mcrobertson09 <84151467+Mcrobertson09@users.noreply.github.com>
2022-05-02 13:54:15 -0700

    new

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 alert('Hello world');
+
+let favFood = 'Pizza';
+let secFavFood = 'Hamburgers'
+
+let person = {
+    name: 'Matthew Robertson',
+    phone: '480-272-1084',
+    city: 'gilbert',
+    state: 'AZ'
+}
+
+document.write("My Favorite food is: ", favFood,  "i also like ", secFavFood, " as a close second");
+document.write(" my name is ", person.name, " I live in ", person.city, ",", person.state)
