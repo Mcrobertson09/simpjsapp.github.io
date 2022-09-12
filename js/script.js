fc440b038d679e4ee4899f4cf544ceff6ef41ed9
Mcrobertson09 <84151467+Mcrobertson09@users.noreply.github.com>
2022-09-12 09:41:01 -0700

    updated

--- js/script.js
+++ js/script.js
@@ -1,47 +1,73 @@
 //IIFE 
 let pokemonRepository = (function (){
-    let pokemonList = [  
-    {id: 1, name: 'bulbasaur', type: ['grass', 'poison'],  HP: 45, Attack: 49, height: 1.0},
-    {id: 2, name: 'squirtle', type: ['water'],  HP: 44, Attack: 49, height: 0.5},
-    {id: 3, name: 'charmander', type:['fire'],  HP: 39, Attack: 52, height: 1.7},
-    {id: 4, name: 'pidgey', type: ['normal', 'flying'],  HP: 35, Attack: 55, height: 1.0}];
-
-    function add(pokemon){
-        if (
-            typeof pokemon === 'object' &&
-            'name' in pokemon
-          ) {
-            pokemonList.push(pokemon);
-          } else {
-            console.log("Enter correct pokemon data");
-          }
-    }
+    fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
+        return response.json(); // This returns a promise!
+    }).then(function (pokemonList) {
+    console.log(pokemonList); // The actual JSON response
+    }).catch(function () {
+    // Error
+    });
 
     function getAll() {
         return pokemonList;
     }
 
+    function addPokemonItem(Poke) {
+        let pokeList = document.querySelector('.pokemon-List');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = Poke.name;
+        button.classList.add('button-class')
+        listItem.appendChild(button);
+        pokeList.appendChild(listItem);
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addPokemonItem: addPokemonItem
     };
 })()
 
-// Calling the GetAll to find respoitory Data
-pokemonRepository.getAll().forEach(Poke => {
-    document.write('</br>' + Poke.name + ': ' 
-                    + '</br>attack: ' + Poke.Attack + ' '
-                    + '</br>type: ' + Poke.type + ' '
-                    + '</br>hp: ' + Poke.HP + ' '
-                    + '</br>height: ' + Poke.height + ' ');
+// let pokemonRepository = (function (){
+//     let pokemonList = [  
+//     {id: 1, name: 'bulbasaur', type: ['grass', 'poison'],  HP: 45, Attack: 49, height: 1.0},
+//     {id: 2, name: 'squirtle', type: ['water'],  HP: 44, Attack: 49, height: 0.5},
+//     {id: 3, name: 'charmander', type:['fire'],  HP: 39, Attack: 52, height: 1.7},
+//     {id: 4, name: 'pidgey', type: ['normal', 'flying'],  HP: 35, Attack: 55, height: 1.0}];
 
-    if (Poke.height >= 1.0) {
-    document.write('- WOW! Thats BIG!!');
-    }
-    document.write('</br>');
-});
+//     function add(pokemon){
+//         if (
+//             typeof pokemon === 'object' &&
+//             'name' in pokemon
+//           ) {
+//             pokemonList.push(pokemon);
+//           } else {
+//             console.log("Enter correct pokemon data");
+//           }
+//     }
 
-document.write('</br>');
+//     function getAll() {
+//         return pokemonList;
+//     }
+
+//     function addPokemonItem(Poke) {
+//         let pokeList = document.querySelector('.pokemon-List');
+//         let listItem = document.createElement('li');
+//         let button = document.createElement('button');
+//         button.innerText = Poke.name;
+//         button.classList.add('button-class')
+//         listItem.appendChild(button);
+//         pokeList.appendChild(listItem);
+//     }
+
+//     return {
+//         add: add,
+//         getAll: getAll,
+//         addPokemonItem: addPokemonItem
+//     };
+// })()
+
 
 // Using the add function to add another pokemon
 pokemonRepository.add({
@@ -50,12 +76,8 @@ pokemonRepository.add({
 
 document.write('</br>');
 
-// Calling the gatAll functrion again to verify it was added
-pokemonRepository.getAll().forEach(Poke => {
-    document.write(`</br>${Poke.name}: </br>ID: ${Poke.id} </br>attack: ${Poke.Attack} </br>type: ${Poke.type} </br>hp: ${Poke.HP} </br>height: ${Poke.height} `);
 
-    if (Poke.height >= 1.0) {
-    document.write('- WOW! Thats BIG!!');
-    }
-    document.write('</br>');
+// Calling the getAll function again to verify it was added
+pokemonRepository.getAll().forEach(Poke => {
+   pokemonRepository.addPokemonItem(Poke);
 });
